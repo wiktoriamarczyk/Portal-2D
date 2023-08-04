80f4cccb835051cf97a022c19c2b1f53284cc217
mkirschniok <kirschniokm@gmail.com>
2023-08-04 17:23:27 +0200

    Improved turret behavior, laser following player, shoot sound

--- Portal-2D/Assets/Scripts/InteractiveObjects/Enemy.cs
+++ Portal-2D/Assets/Scripts/InteractiveObjects/Enemy.cs
@@ -54,19 +54,58 @@ public class Enemy : PickableObject
     /// </summary>
     public AudioClip disabled;
 
+    /// <summary>
+    /// Sound clip to play when the turret is shooting;
+    /// </summary>
+    public AudioClip shoot;
+
+    /// <summary>
+    /// True if the player was previously detected by the turret.
+    /// </summary>
+    public bool wasPlayerDetected = false;
+
+    /// <summary>
+    /// LineRenderer component - for drawing the laser
+    /// </summary>
     private LineRenderer lineRenderer;
+
+    /// <summary>
+    /// SpriteRenderer component - for changing sprites
+    /// </summary>
     private SpriteRenderer spriteRenderer;
+
+    /// <summary>
+    /// Final point of the laser
+    /// </summary>
     Vector3 laserend;
+
+    /// <summary>
+    /// True if the turret wasn't killed
+    /// </summary>
     bool alive = true;
+
+    /// <summary>
+    /// Maximum angle untill tampering is detected
+    /// </summary>
     float maxTiltAngle = 45f;
+
+    /// <summary>
+    /// Auxiliary variable to count time
+    /// </summary>
     float timeSine = 0f;
     
-    void Die()
+    /// <summary>
+    /// Changes the status and sprite of the turret
+    /// </summary>
+    private void Die()
     {
         alive = false;
         spriteRenderer.sprite = inactiveSprite;
     }
 
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
     protected override void Start()
     {
         base.Start();
@@ -77,6 +116,9 @@ public class Enemy : PickableObject
         audioSource = GetComponent<AudioSource>();
     }
 
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
     protected override  void Update()
     {
         base.Update();
@@ -90,26 +132,48 @@ public class Enemy : PickableObject
         {
             StartCoroutine(DieWithDelay());
         }
-        // Narysuj laser (celownik)
-        laserend = transform.position;
-        laserend.x += 10f;
-        laserend.y += 2*Mathf.Sin(timeSine);
-        timeSine += 0.02f;
-        lineRenderer.positionCount = 2;
-        lineRenderer.SetPosition(0, transform.position);
-        lineRenderer.SetPosition(1, laserend);
-        lineRenderer.enabled = true;
-        // SprawdŸ, czy gracz jest w zasiêgu lasera
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, (laserend - transform.position).normalized, Vector3.Distance(transform.position, laserend), layerMask);
-        if (hit.collider != null && hit.collider.gameObject.tag == "Player")
+
+        if (wasPlayerDetected)
         {
-            // odtwórz dŸwiêk
-            if (!audioSource.isPlaying) audioSource.PlayOneShot(detected);
+            // draw a line from the turret to the player
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, transform.position);
+            Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+            playerPosition.y += 2f;
+            lineRenderer.SetPosition(1, playerPosition);
+            lineRenderer.enabled = true;
+            // check if the player is in the line of sight
+            RaycastHit2D hit2 = Physics2D.Raycast(transform.position, (playerPosition - transform.position).normalized, 10f, layerMask);
+            if (hit2.collider != null && hit2.collider.gameObject.tag == "Player")
+            {
+                if (!audioSource.isPlaying) audioSource.PlayOneShot(shoot);
+            }
+            else wasPlayerDetected = false;
         }
-
-
+        if (!wasPlayerDetected)
+        {
+            laserend = transform.position;
+            laserend.x += 10f;
+            laserend.y += 2 * Mathf.Sin(timeSine);
+            timeSine += 0.02f;
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, laserend);
+            lineRenderer.enabled = true;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, (laserend - transform.position).normalized, Vector3.Distance(transform.position, laserend), layerMask);
+            if (hit.collider != null && hit.collider.gameObject.tag == "Player")
+            {
+                // odtwórz dŸwiêk
+                if (!audioSource.isPlaying) audioSource.PlayOneShot(detected);
+                wasPlayerDetected = true;
+            }
+        }
     }
 
+    /// <summary>
+    /// Begins the procedure of dying
+    /// </summary>
+    /// <returns>IEnumerator</returns>
     private IEnumerator DieWithDelay()
     {
         if (!audioSource.isPlaying) audioSource.PlayOneShot(disabled);
